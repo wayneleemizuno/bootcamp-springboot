63c3e6c6fe154c61ed3519a596627db3b78f184c
waynelee <wayne.no.sacrifice.no.victory@gmail.com>
2026-05-05 21:09:44 +0800

    update

--- demo-jpa-database/src/main/java/com/bootcamp/demo/demo_jpa_database/repository/OrderItemRepository.java
+++ demo-jpa-database/src/main/java/com/bootcamp/demo/demo_jpa_database/repository/OrderItemRepository.java
@@ -2,10 +2,9 @@ package com.bootcamp.demo.demo_jpa_database.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.bootcamp.demo.demo_jpa_database.entity.OrderItemEntity;
 
 @Repository
-public interface ItemRepository extends JpaRepository<OrderItemEntity, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItemEntity, Long> {
 
 }
